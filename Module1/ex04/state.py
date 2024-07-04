0d326376d17c941f6cb565001290e49c08c2c7ff
Adrian Gonzalez <adrgonza@c3r1s5.42lausanne.ch>
2024-07-04 11:20:19 +0200

    ex04 finished

---
+++ Module1/ex04/state.py
@@ -0,0 +1,34 @@
+import sys
+
+states = {
+"Oregon" : "OR",
+"Alabama" : "AL",
+"New Jersey": "NJ",
+"Colorado" : "CO"
+}
+capital_cities = {
+"OR": "Salem",
+"AL": "Montgomery",
+"NJ": "Trenton",
+"CO": "Denver"
+}
+
+def search_key_with_value(my_dict, value):
+    for key, val in my_dict.items():
+        if val == value:
+            return key
+    return None
+
+def capital_to_state():
+    if len(sys.argv) != 2:
+        return 0
+    key = search_key_with_value(capital_cities, sys.argv[1])
+    if key:
+        print(search_key_with_value(states, key))
+        return 0
+    print("Unknown capital city")
+    
+    
+
+if __name__ == "__main__":
+    capital_to_state()
