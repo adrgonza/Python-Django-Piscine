928248c6e0493e433cf105d3e5e24600d43e2dd9
Adrian Gonzalez Serrano <adriangonzalezserrano@MacBook-Air-de-Adrian.local>
2024-07-06 13:55:27 +0200

    ex07 finished

--- Module1/ex07/periodic_table.py
+++ Module1/ex07/periodic_table.py
@@ -7,9 +7,40 @@ def write_body(file, elements_dict):
                 <h1>Periodic Table</h1>
             </header>
             <main>
-                <table border = 1>
+                <table class="periodic-table">
     '''
-    for i in range():
+    max_index = max(elements_dict.keys())
+    index = 1
+    for i in range(7):
+        content += '<tr>\n'
+        for j in range(18):
+            while index <= max_index:
+                try :
+                    element = elements_dict[index]
+                    if int(element["position"]) == j:
+                        content += f'''
+                                    <td style="border: 1px solid black; padding:0px">
+                                        <h4>{elements_dict[index]["name"]}</h4>
+                                            <ul>
+                                                <li>{index} {elements_dict[index]["small"]}</li>
+                                                <li>{elements_dict[index]["molar"]}</li>
+                                                <li>{elements_dict[index]["electron"]} electrons</li>
+                                            </ul>
+                                    </td>
+                        '''
+                        index += 1
+                    else:
+                        content += '<td style="border: 1px solid black; padding:10px"></td>'
+                    break
+                except:
+                    index += 1
+        content += '</tr>\n'
+    content += '''
+                </table>
+            </main>
+        </body>
+    </html>
+    '''
     file.write(content)
 
 def write_head(file):
@@ -21,14 +52,36 @@ def write_head(file):
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <title>Periodic Table</title>
             <style>
-                
+                body {
+                    text-align: center;
+                }
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                    border: 1px solid black;
+                    text-align: center;
+                }
+                h4 {
+                    font-size: 11px;
+                    margin-top: 0;
+                    text-align: center;
+                }
+                ul {
+                    list-style-type: none;
+                    padding: 0;
+                    text-align: center;
+                    margin-top: 0px;
+                }
+                li {
+                    font-size: 12px;
+                }
             </style>
         </head>
     '''
     file.write(content)
 
 def fill_html(elements_dict):
-    file = open("./periodic_table.html", w)
+    file = open("./periodic_table.html", "w")
 
     write_head(file)
     write_body(file, elements_dict)
