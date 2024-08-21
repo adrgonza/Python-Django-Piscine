d1734f8bf054989760bd97bc6d210bb5406c5fae
Adrian Gonzalez <adrgonza@c3r1s3.42lausanne.ch>
2024-08-21 19:16:47 +0200

    a lot of changes :))

--- D04/ex02/request_wikipedia.py
+++ D04/ex02/request_wikipedia.py
@@ -5,36 +5,33 @@ import sys
 
 def main():
     if len(sys.argv) != 2:
+        print("You must send an argument")
         return
     
     url = "https://en.wikipedia.org/w/api.php"
     params = {
-        "action": "query",
+        "action": "parse",
         "format": "json",
-        "prop": "extracts",
-        "explaintext": True,
-        "titles": sys.argv[1]
+        "prop": "wikitext",
+        "page": sys.argv[1]
     }
+    
     try:
         response = requests.get(url, params=params)
+        data = response.json()
+        if data.get("error") is not None:
+            print("Page not found")
+            return
+        extract = dewiki.from_string(data["parse"]["wikitext"]["*"])
     except requests.RequestException as e:
         print(f"Error making request to Wikipedia API: {e}")
         return
-    data = response.json()
-    pages = data.get("query", {}).get("pages", {})
-    page = next(iter(pages.values()), {})
-    extract = page.get("extract", "")
 
-    if not extract:
-        print(f"No extract found for page: {sys.argv[1]}")
-        return
-
-    extract = dewiki.from_string(extract)
-    print(extract)
-
-    #with open(sys.argv[1] + ".wiki", "w") as file:
-        #file.write(response)
+    try:
+        with open(sys.argv[1].replace(' ', '_') + ".wiki", "w") as file:
+            file.write(extract)
+    except Exception as e:
+            print(f"An error occurred: {e}")
     
-
 if __name__ == "__main__":
     main()
--- D04/ex03/roads_to_philosophy.py
+++ D04/ex03/roads_to_philosophy.py
@@ -1,12 +1,44 @@
 import sys
-import request
-import BeautifulSoup
+import requests
+from bs4 import BeautifulSoup
+import re
 
 def main():
-    if len(sys.argv) != 2
-        return
+    if len(sys.argv) != 2:
+        return print("You must provide exactly 1 argument.")
+    
+    titles = []
+    url = "https://en.wikipedia.org/wiki/"
+    page = sys.argv[1].replace(' ', '_')
+    
+    while True:
+        content = requests.get(url + page).text
+        soup = BeautifulSoup(content, 'html.parser')
+        title = soup.title.string.replace(" - Wikipedia", "")
         
-    url = "https://en.wikipedia.org/w/api.php"
+        if title in titles:
+            return print("This is an infinite loop!")
+        
+        titles.append(title)
+
+        if title.lower() == "philosophy":
+            print(*titles, sep="\n")
+            return print(f"{len(titles)} steps from {sys.argv[1]} to Philosophy!")
+        
+        text = soup.find(id='mw-content-text')
+        all_links = text.select('p > a')
+
+        if not all_links:
+            return print("It leads to a dead end!")
+        for link in all_links:
+            link_text = link.find_parent('p').text
+            link_text_before = link_text.split(link.text)[0]
+            if link.get('href') and link_text_before.count('(') == link_text_before.count(')'):
+                page = link['href'][6:]
+                break
+        
+        if not page:
+            return print("No valid links found. It leads to a dead end.")
 
 if __name__ == "__main__":
     main()
