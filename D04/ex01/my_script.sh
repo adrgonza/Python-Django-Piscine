f2e22ebef560b6498c32d114d494c3b51bda5d50
Adrian Gonzalez Serrano <adriangonzalezserrano@MacBook-Air-de-Adrian.local>
2024-09-19 23:39:56 +0200

    D04 ex01 fixed

--- D04/ex01/my_script.sh
+++ D04/ex01/my_script.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+# #!/bin/bash
 
 GITURL="https://github.com/jaraco/path.git"
 
-/usr/bin/python3 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
-python -m pip --version
+echo "Pip version:"
+pip --version
 
-python -m pip install --log pip_install.log --force-reinstall git+$GITURL
+pip install --target="local_lib" --log path_install.log --force-reinstall git+"$GITURL"
 
-python3 my_program.py
+python my_program.py
